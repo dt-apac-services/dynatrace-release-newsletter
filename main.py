dd451f989037f407c34fc794a57cc9d892fb0520
Arun Krishnan <arun.krishnan@dynatrace.com>
2022-10-12 11:17:16 +1300

    release versions & links stored locally

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import copy
 import json
 import logging
+import math
 import os
 from pathlib import Path
+import scripts.release_notes as release_notes
 
+# global release_info_from_file
 
 def main():    
-    # Setup logger
-    logger = logging.getLogger(__name__)
+    # Setup logger        
     release_info_from_file = read_release_info_file()
     #print(release_info_from_file["Dynatrace SaaS"])
+
+    # Scrape release note page to find new releases
+    release_info = copy.deepcopy(release_info_from_file)                                # Required to prevent reference
+    release_info_from_web = release_notes.get_latest_versions(release_info)
+
+    for c in release_info_from_web:
+        for v, l in release_info_from_web[c].items():            
+            # for x,y in release_info_from_file[c].items():
+            if v not in release_info_from_file[c]:
+                print("New Version Available for "+ c)
+                print(v+":"+l)
+                #print(c+": "+v)
+                # Kick off gathering of info
+
+    
+    write_release_info_to_local_file(release_info_from_web)
 
 # Read local release info file
 def read_release_info_file():
@@ -17,7 +36,9 @@ def read_release_info_file():
     
     # Create file if not present
     if not os.path.exists(local_release_info_file):
-        new_file = open(local_release_info_file,"w")
+        dict = {"Dynatrace SaaS":{},"Dynatrace Managed":{},"Dynatrace OneAgent":{},"Dynatrace ActiveGate":{},"Dynatrace Cloud":{}}
+        new_file = open(local_release_info_file,"w")        
+        new_file.write(json.dumps(dict, indent=4))    
         new_file.close()
 
     # Open local release info file and read content
@@ -28,6 +49,15 @@ def read_release_info_file():
         release_info_from_file= json.loads(content)
     f.close()
     return release_info_from_file
+
+
+# Write into local release info file
+def write_release_info_to_local_file(release_info_from_web):
+    #local_release_info_file = Path(__file__).parent/"release_info.json"
+    local_release_info_file = "C:\\Users\\arun.krishnan\\OneDrive - Dynatrace\\Projects\\github\\dynatrace-release-newsletter\\release_info.json"
+    f = open(local_release_info_file,"w")
+    f.write(json.dumps(release_info_from_web, indent=4))
+    f.close()
 
 
 if __name__ == "__main__":
--- scripts/release_notes.py
+++ scripts/release_notes.py
@@ -3,33 +3,31 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-## Read Release notes page
-URL="https://www.dynatrace.com/support/help/whats-new/release-notes"    
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
 
-results = soup.find_all(title=re.compile("Release notes*"))
+def get_latest_versions(release_info):
 
-json_file = "C:\\Users\\arun.krishnan\\OneDrive - Dynatrace\\Projects\\github\\dynatrace-release-info\\data\\release_info.json"
-# Read from json file
-with open(json_file, 'r') as openfile:    
-    release_info = json.load(openfile)
+    ## Read Release notes page
+    URL="https://www.dynatrace.com/support/help/whats-new/release-notes"    
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-for i in results:
-    component = re.findall("(Dynatrace\s[\w]+)",i['title'])[0]
-    version = re.findall("[version|release]\s*([\d.]+)",i['title'])[0]
-    link = "https://dynatrace.com"+i['href']
+    results = soup.find_all(title=re.compile("Release notes*"))
 
-    if component not in release_info:
-        release_info[component]=[]
-    if version not in release_info[component]:
-        release_info[component].append(version)
-        # trigger creation of report
+    for i in results:
+        component = re.findall("(Dynatrace\s[\w]+)",i['title'])[0]
+        version = re.findall("[version|release]\s*([\d.]+)",i['title'])[0]
+        link = "https://dynatrace.com"+i['href']        
     
-json_data = json.dumps(release_info, indent=4)
-# Writing to release_info.json
-with open(json_file, "w") as outfile:
-    outfile.write(json_data)
+        if component not in release_info:
+            release_info[component]={}
+        if version not in release_info[component]:            
+            release_info[component][version] = link        
+    
+    return release_info
+
+def scrape_specific_release_page():
+    URL="x"
+    
     
     
     
